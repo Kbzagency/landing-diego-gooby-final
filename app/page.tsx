d02b4692a8e59af21e1ecdfd5fe063e2b979c386
cesesteban <ces.esteban@gmail.com>
2026-04-16 17:15:45 -0300

    Eliminar SectionDivider entre slider y CTA para acercar el texto

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,6 @@ export default function Page() {
         <VisionNegocioSection />
         <SectionDivider />
         <SliderSection />
-        <SectionDivider />
         <CTASection />
       </main>
     </>
